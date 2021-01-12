3850a10a838b3b6cb0a105663ffde779df303475
dino.seo <dino.seo@kakaocorp.com>
2021-01-12 19:16:03 +0900

    test 03

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// test 01
 // test 02
+// test 03
 ReactDOM.render(
   <React.StrictMode>
     <App />
